bbc52c2946688669ae365a0fa75786686989b27a
Sreeranjani Didugu <didugu@wisc.edu>
2020-08-15 03:41:12 -0500

    Update train_pix2pix.sh

--- scripts/train_pix2pix.sh
+++ scripts/train_pix2pix.sh
@@ -1,2 +1,3 @@
+#[Reference citation]
 set -ex
 python train.py --dataroot ./datasets/facades --name facades_pix2pix --model pix2pix --netG unet_256 --direction BtoA --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0
