272689c4d036a77e367214a6912061a64ed69b21
Sreeranjani Didugu <didugu@wisc.edu>
2020-08-15 03:40:20 -0500

    Update test_single.sh

--- scripts/test_single.sh
+++ scripts/test_single.sh
@@ -1,2 +1,3 @@
+#[Reference citation]
 set -ex
 python test.py --dataroot ./datasets/facades/testB/ --name facades_pix2pix --model test --netG unet_256 --direction BtoA --dataset_mode single --norm batch
